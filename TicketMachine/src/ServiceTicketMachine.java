d7c884ed20d5d08f76f3e8e892c5ac250c46e2c3
AkilaPrathap <131250575+AkilaPr@users.noreply.github.com>
2023-12-26 00:00:20 +0530

    Edits

--- TicketMachine/src/ServiceTicketMachine.java
+++ TicketMachine/src/ServiceTicketMachine.java
@@ -1,2 +1,17 @@
 public interface ServiceTicketMachine {
+
+    public final int FULL_PAPER_TRAY = 500;
+
+    public final int SHEET_PER_PACK = 250;
+
+    public final int MIN_PAPER_LEVEL = 3;
+
+    
+    public final int FULL_TONER_LEVEL        = 500 ;
+
+    public final int Minimum_Toner_Level     = 3 ;
+
+    public final int PagesPerTonerCartridge  = 500 ;
+
+
 }
